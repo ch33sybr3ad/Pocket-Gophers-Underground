0807c4b4d485c27f9132bf191bf4ffb95d7406a5
Andrew Carlson <acarl005@g.ucla.edu>
2015-06-23 15:46:24 -0700

    fix redis client auth

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var io = require('socket.io')(server);
 if(process.env.REDISTOGO_URL) {
   var rtg = require('url').parse(process.env.REDISTOGO_URL);
   var redisClient = redis.createClient(rtg.port, rtg.hostname);
-  redis.auth(rtg.auth.split(':')[1]);
+  redisClient.auth(rtg.auth.split(':')[1]);
 } else {
   var redisClient = redis.createClient();
 }
