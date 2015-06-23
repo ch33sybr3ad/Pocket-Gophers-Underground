25bb763f8f8da86caa7afc203301de001c147e50
Andrew Carlson <acarl005@g.ucla.edu>
2015-06-23 14:49:15 -0700

    fix port

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var redis = require('redis')
+var port = process.env.PORT || 8000
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -170,7 +171,7 @@ function resetGame() {
   ]
 }
 
-server.listen(8000);
+server.listen(port);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
